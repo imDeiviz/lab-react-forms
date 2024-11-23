5ebd0a7f06565e139c55a0b4d6f0ac83a6a40199
David Rojas <dmrojassantana@gmail.com>
2024-11-23 14:40:27 +0100

    done

---
+++ src/components/AddStudent.jsx
@@ -0,0 +1,129 @@
+import { useState } from 'react';
+
+const AddStudent = ({ handleAddStudent }) => {
+  const [formData, setFormData] = useState({
+    fullName: "",
+    email: "",
+    phone: "",
+    program: "Web Dev",
+    image: "",
+    graduationYear: 2023,
+    graduated: false,
+  });
+
+  const handleChange = (event) => {
+    const { name, value, type, checked } = event.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: type === 'checkbox' ? checked : value,
+    }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    handleAddStudent(formData);
+
+    setFormData({
+      fullName: "",
+      email: "",
+      phone: "",
+      program: "Web Dev",
+      image: "",
+      graduationYear: 2023,
+      graduated: false,
+    });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <span>Add a Student</span>
+      <div>
+        <label>
+          Full Name
+          <input
+            name="fullName"
+            type="text"
+            placeholder="Full Name"
+            value={formData.fullName}
+            onChange={handleChange}
+          />
+        </label>
+
+        <label>
+          Profile Image
+          <input
+            name="image"
+            type="url"
+            placeholder="Profile Image"
+            value={formData.image}
+            onChange={handleChange}
+          />
+        </label>
+
+        <label>
+          Phone
+          <input
+            name="phone"
+            type="tel"
+            placeholder="Phone"
+            value={formData.phone}
+            onChange={handleChange}
+          />
+        </label>
+
+        <label>
+          Email
+          <input
+            name="email"
+            type="email"
+            placeholder="Email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+        </label>
+      </div>
+
+      <div>
+        <label>
+          Program
+          <select
+            name="program"
+            value={formData.program}
+            onChange={handleChange}
+          >
+            <option value="Web Dev">Web Dev</option>
+            <option value="UXUI">UXUI</option>
+            <option value="Data">Data</option>
+          </select>
+        </label>
+
+        <label>
+          Graduation Year
+          <input
+            name="graduationYear"
+            type="number"
+            placeholder="Graduation Year"
+            min={2023}
+            max={2030}
+            value={formData.graduationYear}
+            onChange={handleChange}
+          />
+        </label>
+
+        <label>
+          Graduated
+          <input
+            name="graduated"
+            type="checkbox"
+            checked={formData.graduated}
+            onChange={handleChange}
+          />
+        </label>
+
+        <button type="submit">Add Student</button>
+      </div>
+    </form>
+  );
+};
+
+export default AddStudent;
